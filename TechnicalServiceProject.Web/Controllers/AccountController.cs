e499193fef40785cb43fabfc916c9b3231ce689a
elifboluk <elifbolukk@gmail.com>
2022-05-19 12:50:08 +0300

    Header oluşturuldu.

---
+++ TechnicalServiceProject.Web/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechnicalServiceProject.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Register() 
+        { 
+            return View(); 
+        }
+
+    }
+}
